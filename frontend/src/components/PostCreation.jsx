1d61eb6a2dd8953494ba4142a3d913aba4dcc0f1
praveen8240 <156305741+praveen8240@users.noreply.github.com>
2024-11-11 21:42:20 +0530

    mongo

--- frontend/src/components/PostCreation.jsx
+++ frontend/src/components/PostCreation.jsx
@@ -83,13 +83,13 @@ const PostCreation = ({ user }) => {
 			)}
 
 			<div className='flex justify-between items-center mt-4'>
-				<div className='flex space-x-4'>
+				{/* <div className='flex space-x-4'>
 					<label className='flex items-center text-info hover:text-info-dark transition-colors duration-200 cursor-pointer'>
 						<Image size={20} className='mr-2' />
 						<span>Photo</span>
 						<input type='file' accept='image/*' className='hidden' onChange={handleImageChange} />
 					</label>
-				</div>
+				</div> */}
 
 				<button
 					className='bg-primary text-white rounded-lg px-4 py-2 hover:bg-primary-dark transition-colors duration-200'
